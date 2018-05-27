45c8c794b314adb0e1909cea3ef22ad245e04d81
sara3laa <sara3laa.93@gmail.com>
2018-05-27 05:38:41 +0200

    Complete authentication system

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+      user = User.find_by(email: params[:session][:email].downcase)
+      if user && user.authenticate(params[:session][:password])
+          session[:user_id]= user.id
+          flash[:success]="welcome to your Blog"
+          redirect_to users_path(user)
+      else
+        flash.now[:danger]= "yourlogin information are incorrect"
+        render 'new'
+      end
+  end
+
+  def destroy
+    session[:user_id]=nil
+    flash[:success] = "you have logged out"
+    redirect_to root_path
+  end
+
+
+
+
+
+
+
+end
