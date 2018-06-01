5f1dd73a8406edf92ec1adf5289c716db767c63e
sara3laa <sara3laa.93@gmail.com>
2018-06-01 04:58:18 +0200

    Add admin requirments to create categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
   def index
-    @categories = Category.paginate(page: params[:page],per_page: 5)
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def new
@@ -25,5 +26,12 @@ class CategoriesController < ApplicationController
 
   def category_params
     params.require(:category).permit(:name)
+  end
+
+  def require_admin
+    if !logged_in? || (logged_in? && !current_user.admin?)
+      flash[:danger] = 'Only admins can perform that action'
+      redirect_to categories_path
+    end
   end
 end
