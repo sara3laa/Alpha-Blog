051371f1872f80a493dfc03a88384a20895a717c
sara3laa <sara3laa.93@gmail.com>
2018-06-01 07:43:35 +0200

    Add edit category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,7 @@ class CategoriesController < ApplicationController
       render 'new'
     end
   end
+
   def edit
     @category = Category.find(params[:id])
   end
@@ -25,12 +26,13 @@ class CategoriesController < ApplicationController
   def update
     @category = Category.find(params[:id])
     if @category.update(category_params)
-      flash[:success] = "Category name was Successfully updated..."
+      flash[:success] = 'Category name was Successfully updated...'
       redirect_to category_path(@category)
     else
       render 'edit'
     end
   end
+
   def show
     @category = Category.find(params[:id])
     @category_articles = @category.articles.paginate(page: params[:page], per_page: 5)
